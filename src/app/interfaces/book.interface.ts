624fb4772fbd8a053c08758f0fbaf074fb155a63
Ria Pacheco <ria.pacheco@coldboretechnology.com>
2022-09-22 18:34:40 -0600

    Defined types w/ interfaces moving on to models

---
+++ src/app/interfaces/book.interface.ts
@@ -0,0 +1,65 @@
+import { IPassage } from "./passage.interface";
+
+/**
+ * Interface that defines the {@link Book} object's data types
+ */
+export interface IBook {
+  id: number;
+  title: string;
+  passages: IPassage[];
+  bookScores: IBookScoreCategory[];
+}
+
+/**
+ * Interface for Ref Table that holds the ID of a {@link Book} 
+ * and the ID of an associated {@link Passage}
+ * Relationship is 1-to-many
+ */
+export interface IBookPassagesRelation {
+  id: number;
+  book: number;
+  passage: number;
+}
+
+/**
+ * Interface for Ref Table that holds the ID of a {@link Book} 
+ * and the ID of a related {@link BookGenre}
+ * Relationship is 1-to-many
+ */
+export interface IBookGenresRelation {
+  id: number;
+  book: number;
+  genre: number;
+}
+/**
+ * Interface for Ref Table that holds the ID of a {@link Book} 
+ * and the ID of a related {@link BookScoreCategory}
+ * Relationship is 1-to-5
+ */
+export interface IBookScoreCategoriesRelation {
+  id: number;
+  book: number;
+  scoreCategory: number;
+}
+
+/**
+ * Interface that defines the {@link BookGenre} 's data types
+ */
+export interface IBookGenre {
+  id: number;
+  label: string;
+  target?: string;
+  description?: string;
+}
+
+/**
+ * Interface that defines the {@link BookScoreCategory} 's data types
+ */
+export interface IBookScoreCategory {
+  id: number;
+  label: string;
+  potentialPoints: number;
+  value?: number;
+  target?: string;
+  description?: string;
+}
--- src/app/interfaces/passage.interface.ts
+++ src/app/interfaces/passage.interface.ts
@@ -1,18 +1,55 @@
+
+
+/**
+ * Interface that defines the {@link Passage}'s data types
+ */
 export interface IPassage {
   id: number;
   body: string;
-  bookId?: any;
+  dateCreated: Date;
+  tags?: IPassageTag[];
+  notes?: IPassageNote[];
   pageNumber?: number;
-
-  
 }
 
+/**
+ * Interface for Ref Table that holds the ID of a {@link Passage} 
+ * and the ID of a corresponding {@link PassageTag}
+ * Relationship is one-to-many
+ */
+export interface IPassageTagsRelation {
+  id: number;
+  passage: number;
+  passageTag: number;
+}
+
+/**
+ * Interface for Ref Table that holds the ID of a {@link Passage} 
+ * and the ID of a corresponding {@link PassageNote}
+ * Relationship is one-to-many
+ */
+export interface IPassageNotesRelation {
+  id: number;
+  passage: number;
+  passageNote: number;
+}
+
+/**
+ * Interface that defines the {@link PassageTag}'s data types
+ */
 export interface IPassageTag {
   id: number;
+  label: string;
+  target?: string;
+  description?: string;
 }
-
+/**
+ * Interface that defines the {@link PassageNote}'s data
+ */
 export interface IPassageNote {
   id: number;
-  
+  body: string;
+  dateCreated: Date;
+  target?: string;
+  description?: string;
 }
-
